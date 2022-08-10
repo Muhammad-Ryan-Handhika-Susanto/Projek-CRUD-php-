1dd409370de3ac2949b317ac55f73a1679f87bec
Muhammad-Ryan-Handhika-Susanto <ryanhandika712@gmail.com>
2022-08-10 10:04:42 +0700

    Mengubah Syntax

--- Projek CRUD/ubah.php
+++ Projek CRUD/ubah.php
@@ -47,7 +47,7 @@ if(isset($_POST['submit']) ) {
 <html>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <head>
-    <title>update Data Siswa</title>
+    <title>Update Data Siswa</title>
     <style>
         body {
             background-color: saddlebrown;
